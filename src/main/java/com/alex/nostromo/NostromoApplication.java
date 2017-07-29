f830c01112729bd2bf6b93b88673c83ed623822f
alex-gab <dragoi.alex@gmail.com>
2017-07-29 12:44:43 +0300

    [enh]: Initial commit

---
+++ src/main/java/com/alex/nostromo/NostromoApplication.java
@@ -0,0 +1,12 @@
+package com.alex.nostromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NostromoApplication.class, args);
+	}
+}
